004b5c321ae4badd95f7dfb9e790ed6a45000c5b
igoriury1111 <zzz.igoriury@gmail.com>
2025-11-17 16:22:21 -0300

    Add files via upload

---
+++ atividade1/aula1_tabuada/Untitled-1.php
@@ -0,0 +1,16 @@
+//header.php
+<!DOCTYPE html>
+<html>
+   <head><title>meu site</title></head>
+<body>
+    <h1>bem vindo!</h1>
+
+//footer.php
+<footer> 2024 meu site</footer>
+</body></html>
+
+//index.php
+<?php
+require_once'header.php';
+echo "<p>conteudo da pagina</p>";
+require_once'footer.php';
